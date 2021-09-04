eeb834a8c2fa4ee76bd8056fffbfb3e73e3bbce5
Sharif <sharibeast@gmail.com>
2021-09-04 16:04:13 +0300

    fix tailwind config purge path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   // mode: 'jit',
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
 
   darkMode: false, // or 'media' or 'class'
   theme: {
